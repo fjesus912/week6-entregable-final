9b7c0c4b3eb377a44b0f8506e417c708c99980f9
Jesús González <fjesus1209@gmail.com>
2024-02-19 22:04:10 -0600

    advances

---
+++ src/routes/user.router.js
@@ -0,0 +1,17 @@
+const { getAll, create, getOne, remove, update, login } = require('../controllers/user.controllers');
+const express = require('express');
+
+const routerUser = express.Router();
+
+routerUser.route('/')
+    .get(getAll)
+    .post(create);
+
+routerUser.route('/login')
+    .post(login)
+
+routerUser.route('/:id')
+    .delete(remove)
+    .put(update);
+
+module.exports = routerUser;
