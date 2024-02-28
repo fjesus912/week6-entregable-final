5c6996600bbbd1a08ae5fa150aa831ec0e0201ab
Jesús González <fjesus1209@gmail.com>
2024-02-27 20:09:10 -0600

    advances

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -2,6 +2,7 @@ const catchError = require("../utils/catchError");
 const Cart = require("../models/Cart");
 const Product = require("../models/Product");
 const Category = require("../models/Category");
+const ProductImg = require("../models/ProductImg");
 
 const getAll = catchError(async (req, res) => {
   const userId = req.user.id;
@@ -11,10 +12,15 @@ const getAll = catchError(async (req, res) => {
       {
         model: Product,
         attributes: { exclude: ["updatedAt", "createdAt"] },
-        include: {
-          model: Category,
-          attributes: ["name"],
-        },
+        include: [
+          {
+            model: Category,
+            attributes: ["name"],
+          },
+          {
+            model: ProductImg,
+          },
+        ],
       },
     ],
   });
@@ -22,7 +28,7 @@ const getAll = catchError(async (req, res) => {
 });
 
 const getOne = catchError(async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params;
   const userId = req.user.id;
   const results = await Cart.findByPk(id, {
     where: { userId },
@@ -30,10 +36,15 @@ const getOne = catchError(async (req, res) => {
       {
         model: Product,
         attributes: { exclude: ["updatedAt", "createdAt"] },
-        include: {
-          model: Category,
-          attributes: ["name"],
-        },
+        include: [
+          {
+            model: Category,
+            attributes: ["name"],
+          },
+          {
+            model: ProductImg,
+          },
+        ],
       },
     ],
   });
